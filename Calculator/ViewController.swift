661fdc61faa1d26171662e9686b45f8211af6541
MasaCode <masacode.vancouver@gmail.com>
2019-10-15 14:47:58 +0900

    Create a project

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Masashi Morita on 2019/10/15.
+//  Copyright © 2019 Masashi Morita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
